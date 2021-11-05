91be1368ba3eff33189a44d353da91191e0e461c
SuddenlyNothing <72772195+SuddenlyNothing@users.noreply.github.com>
2021-11-05 13:52:41 -0700

    Fix InputFormatLabel loading not formatting text

--- BaseProject/Scenes/UI/InputFormatLabel.gd
+++ BaseProject/Scenes/UI/InputFormatLabel.gd
@@ -2,6 +2,11 @@ extends Label
 
 onready var unformatted_text = text
 
+
+func _ready() -> void:
+	update_keys()
+
+
 # Formats the text.
 # Gets called by InputRemap and InputRemapButton when the keys
 # for the buttons change or the keys are being loaded.
