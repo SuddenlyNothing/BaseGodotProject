aae21507e3491ca75d6b45cd61ff707403f105ab
SuddenlyNothing <72772195+SuddenlyNothing@users.noreply.github.com>
2022-02-05 12:01:55 -0800

    Add input remapping and auto remap button font resizing

--- BaseProject2/options/InputRemap/InputRemap.gd
+++ BaseProject2/options/InputRemap/InputRemap.gd
@@ -15,14 +15,18 @@ const MOUSE_BUTTONS = {
 	BUTTON_WHEEL_RIGHT: "BUTTON_WHEEL_RIGHT", # Mouse wheel right button
 }
 
+# Used for determining key button font size
+const BUTTON_H_CONTENT_MARGIN : int = 30
+const DEFAULT_FONT_SIZE : int = 32
+
 # True when a key is being remapped
 var is_mapping : bool = false
-# Button being remapped
+# Button being remapped set on remap button pressed
 var map_key_button := {
 	"self": null,
 	"parent": null,
-	"map_action": null,
-	"key_button_ind": null,
+	"action": null,
+	"ind": null,
 }
 
 
@@ -38,8 +42,25 @@ func load_data() -> void:
 			add_new_input_remap_module(action, InputMap.get_action_list(action), false)
 
 
+# Maps new inputs to actions using map_key_button
 func _input(event: InputEvent) -> void:
-	input_to_text(event)
+	if is_mapping:
+		if can_map_event(event):
+			map_key_button.self.text = input_to_text(event)
+			var action_inputs : Array = Save.data.actions[map_key_button.action].inputs
+			var old_event : InputEvent = action_inputs[map_key_button.ind]
+			# replace old event
+			action_inputs[map_key_button.ind] = event
+			# Map action to new event
+			InputMap.action_erase_event(map_key_button.action, old_event)
+			InputMap.action_add_event(map_key_button.action, event)
+			# Set font size
+			set_key_button_font_size(map_key_button.self, input_to_text(event))
+			# Reset mapping state.
+			is_mapping = false
+			map_key_button.self.pressed = false
+		# Eat inputs so it doesn't propogate further
+		get_tree().set_input_as_handled()
 
 
 # Adds an input remap module as a child
@@ -72,17 +93,44 @@ func add_new_input_remap_module(action: String, events: Array, from_save: bool =
 func add_new_key_button(parent, event: InputEvent, action: String, button_ind: int) -> void:
 	var key_button := ButtonSFX.new()
 	key_button.text = input_to_text(event)
+#	key_button.add_font_override("roboto-regular", preload("res://assets/fonts/RobotoRegular32.tres"))
 	key_button.toggle_mode = true
 	key_button.clip_text = true
 	key_button.connect("pressed", self, "_on_key_button_pressed",
 			[parent, key_button, action, button_ind])
 	parent.add_child(key_button)
+	yield(key_button, "draw")
+	key_button.rect_min_size = key_button.rect_size
+	set_key_button_font_size(key_button, input_to_text(event))
 
 
+# Update font size to fit text within button
+func set_key_button_font_size(key_button: ButtonSFX, text: String) -> void:
+	var font : DynamicFont = key_button.get_font("name")
+	var content_size = key_button.rect_size.x - BUTTON_H_CONTENT_MARGIN
+	var font_lines = font.get_string_size(text).x / content_size
+	# Couldn't find a better way to change font size for single button
+	var path = "res://assets/fonts/Roboto-Regular.ttf"
+	var new_font = DynamicFont.new()
+	var new_data = DynamicFontData.new()
+	var new_font_size : int = floor(min(font.size / font_lines, DEFAULT_FONT_SIZE))
+	new_data.font_path = path
+	new_font.font_data = new_data
+	new_font.size = new_font_size
+	key_button.set("custom_fonts/font", new_font)
+
+
+# Sets the mapping state to active starts the input detection
 func _on_key_button_pressed(parent, key_button: ButtonSFX, action: String, button_ind: int) -> void:
 	if not key_button.pressed:
 		return
-	print(is_children_defaults(parent, action))
+	is_mapping = true
+	map_key_button.self = key_button
+	map_key_button.parent = parent
+	map_key_button.action = action
+	map_key_button.ind = button_ind
+	key_button.text = "..."
+	set_key_button_font_size(key_button, "...")
 
 
 # Checks if children of parent with the action child has the default inputs
