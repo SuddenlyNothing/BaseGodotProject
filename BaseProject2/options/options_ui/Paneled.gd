1a0ee219efd12abef4809cb9fd9bba347917beef
SuddenlyNothing <72772195+SuddenlyNothing@users.noreply.github.com>
2022-07-14 19:45:13 -0700

    Fix paneled exit mouse detection

--- BaseProject2/options/options_ui/Paneled.gd
+++ BaseProject2/options/options_ui/Paneled.gd
@@ -1,11 +1,23 @@
 extends Control
 
+var is_mouse_inside := false
+
 onready var settings_manager := $M/V
 onready var pressed_sfx := $PressedSFX
 
 
 func _on_ColorRect_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if event.button_index == 1 and not event.is_pressed():
-			settings_manager._on_Back_pressed()
-			pressed_sfx.play()
+		if event.button_index == 1:
+			yield(get_tree(), "idle_frame")
+			if not event.is_pressed() and is_mouse_inside:
+				settings_manager._on_Back_pressed()
+				pressed_sfx.play()
+
+
+func _on_ColorRect_mouse_entered() -> void:
+	is_mouse_inside = true
+
+
+func _on_ColorRect_mouse_exited() -> void:
+	is_mouse_inside = false
